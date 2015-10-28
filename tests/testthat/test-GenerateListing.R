f312f8b4239f2ada73ff8e4951ef75d056a6df33
Jared Lander <info@jaredlander.com>
2015-10-28 17:43:54 -0400

    Testing if research generates correctly.

--- tests/testthat/test-GenerateListing.R
+++ tests/testthat/test-GenerateListing.R
@@ -4,12 +4,20 @@ oneJob <- jobs %>% filter(JobName=='Tech Startup', Company=='Pied Piper')
 noBullets <- generateListing(oneJob)
 withBullets <- generateListing(oneJob, bullets=c(1, 3))
 
+oneResearch <- jobs %>% filter(JobName=='Garfield Research', Company=='Hudson University')
+researchOne <- generateListing(oneResearch, bullets=4, type='Research')
+researchTwo <- generateListing(oneResearch, bullets=4:5, type='Research')
+
 test_that("generateListing returns only one element of text", {
     expect_equal(length(noBullets), 1)
     expect_equal(length(withBullets), 1)
+    expect_equal(length(researchOne), 1)
+    expect_equal(length(researchTwo), 1)
     
     expect_is(noBullets, 'character')
     expect_is(withBullets, 'character')
+    expect_is(researchOne, 'character')
+    expect_is(researchTwo, 'character')
 })
 
 test_that('generateListing builds the right text', {
@@ -20,4 +28,12 @@ test_that('generateListing builds the right text', {
     expect_equal(withBullets,
                  "\\begin{rSubsection}{Pied Piper}{2013--Present}{CTO}{New York, NY}\n\\item Set up company's computing platform\n\\item Constructed statistical models for predictive analytics of big data\n\\end{rSubsection}"
     )
+    
+    expect_equal(researchOne,
+                 "\\begin{Research}{Hudson University}{Humanitarian Research}{2008--Present}\n\\item To reduce the time, cost and effort necessary to properly analyze a region affected by a natural or manmade disaster Dr. Richard Garfield and I worked on methods to show that smaller sample sizes can adequately provide the same information as larger sample sizes.  For our study we used data from the 2010 Pakistan floods that devastated large swathes of the country.  We compared varying samples sizes ranging from 5 tehsils (villages) in each province to full enumeration.  While the research is still ongoing we have so far demonstrated that as few as 10 tehsils per province is sufficient.  This should help improve future responses to disasters.\n\\end{Research}"
+    )
+    
+    # expect_equal(researchTwo,
+    #              "\\begin{Research}{Hudson University}{Humanitarian Research}{2008--Present}\n\\item To reduce the time, cost and effort necessary to properly analyze a region affected by a natural or manmade disaster Dr. Richard Garfield and I worked on methods to show that smaller sample sizes can adequately provide the same information as larger sample sizes.  For our study we used data from the 2010 Pakistan floods that devastated large swathes of the country.  We compared varying samples sizes ranging from 5 tehsils (villages) in each province to full enumeration.  While the research is still ongoing we have so far demonstrated that as few as 10 tehsils per province is sufficient.  This should help improve future responses to disasters.\n\\item I was engaged in statistical and analytic support to Dr. Garfield of Columbia’s School of Nursing and Dr. John Mutter from Columbia’s Earth Institute in their study of the socioeconomic, health and geographic factors affecting deaths in New Orleans following Hurricane Katrina. We are reconciling a self-reporting death list with an official State list that is thought to under represent Katrina related deaths. The goal is to determine what disparities, if any, resulted in a greater proportion of deaths among certain populations as opposed to others. The analysis will result in journal article preparation in in which I will be one of the authors.\n\\end{Research}"
+    # )
 })
