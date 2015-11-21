a94744206c92ed14c3bd469f13ea4c4680c63c90
Jared Lander <info@jaredlander.com>
2015-11-20 20:52:54 -0500

    Documentation for jobs data.

---
+++ R/jobs-data.r
@@ -0,0 +1,19 @@
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the listings for a resume
+#'
+#' @format A data frame with 27 rows and 10 variables:
+#' \describe{
+#'   \item{JobName}{The internal name given to ID the job}
+#'   \item{Company}{Name of company}
+#'   \item{Location}{Job Location}
+#'   \item{Title}{Position Title}
+#'   \item{Start}{Start date of job}
+#'   \item{End}{End date of job}
+#'   \item{Bullet}{Bullet points for jobs}
+#'   \item{BulletName}{Name or ID for bullets}
+#'   \item{Type}{Type of job, either a job or research}
+#'   \item{Description}{Short blurb about the job}
+#' }
+#' @source Manufactured data
+"jobs"
